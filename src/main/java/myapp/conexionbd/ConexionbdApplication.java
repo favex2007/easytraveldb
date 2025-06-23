d2e5fcd1d85210d2f2e65598c125d0dda78abd06
facundovegacruz <vegacruzfacundo@gmail.com>
2025-06-22 21:50:47 -0300

    XD

---
+++ src/main/java/myapp/conexionbd/ConexionbdApplication.java
@@ -0,0 +1,13 @@
+package myapp.conexionbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexionbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexionbdApplication.class, args);
+	}
+
+}
